e5a16528fc0fdd29f400357f4f0d6a1694f96d9b
KDSigel <heykarl@gmail.com>
2022-02-14 14:43:34 -0800

    added test for get by id and made method

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,5 +1,8 @@
 const fs = require('fs/promises');
 const path = require('path');
+const SimpleDb = require('../lib/simple-db');
+const shortid = require('shortid');
+
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
@@ -12,7 +15,19 @@ describe('simple database', () => {
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('needs a first test...', async () => {
+  it('Returns an object deserialized (JSON.parse) from the contents of the file with that id in the directory', async () => {
+
+    const firstDb = new SimpleDb(TEST_DIR);
+    const shortId = shortid.generate();
+    const newObject = {
+      name: 'test name',
+      text: 'I do not follow',
+      id: shortId,
+    };
+    fs.writeFile(TEST_DIR + '/' + shortId + '.txt', JSON.stringify(newObject));
+
+    const result = await firstDb.get(newObject.id);
+    expect(result).toEqual({ name: 'test name', text: 'I do not follow', id: shortId });
 
   });
 
